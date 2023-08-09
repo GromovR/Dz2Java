0e5ac3c733c423247bb471575524186a22ef33af
Gromov Roman <romzes.rz@gmail.com>
2023-08-09 21:31:06 +0300

    correct1

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         int money = 300; //количество денег вносимых на счет
-        int bonus = 100;
+        int cash = 100;
         if (money < 1000) {
-            System.out.println(money + bonus);
+            System.out.println(money + cash);
         } else  {
-            System.out.println(money / 100 + money + bonus);
+            System.out.println(money / 100 + money + cash);
         }
     }
 }
